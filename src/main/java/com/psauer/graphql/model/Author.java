7c399fed35e90c7ed941a4ab0c2fbd0f511af60f
Patricia Sauer <patricia.sauer@maibornwolff.de>
2018-11-11 21:14:13 +0100

    Added entities for author and book + lombok dependency

---
+++ src/main/java/com/psauer/graphql/model/Author.java
@@ -0,0 +1,33 @@
+package com.psauer.graphql.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    public Author() {
+    }
+
+    public Author(Long id) {
+        this.id = id;
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+}
---
+++ src/main/java/com/psauer/graphql/model/Book.java
@@ -0,0 +1,40 @@
+package com.psauer.graphql.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+
+    private String isbn;
+
+    private int pageCount;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id",
+        nullable = false, updatable = false)
+    private Author author;
+
+    public Book() {
+    }
+
+    public Book(String title, String isbn, int pageCount, Author author) {
+        this.title = title;
+        this.isbn = isbn;
+        this.pageCount = pageCount;
+        this.author = author;
+    }
+
+}
