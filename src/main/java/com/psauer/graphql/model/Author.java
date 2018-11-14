358fbb89fb3efee472374f473196398cb3126657
Patricia Sauer <patricia.sauer@maibornwolff.de>
2018-11-14 19:14:06 +0100

    Added Configuration and GraphQLErrorAdapter

--- src/main/java/com/psauer/graphql/model/Author.java
+++ src/main/java/com/psauer/graphql/model/Author.java
@@ -4,12 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Entity
 @Data
 @Builder
+@AllArgsConstructor
 public class Author {
 
     @Id
--- src/main/java/com/psauer/graphql/model/Book.java
+++ src/main/java/com/psauer/graphql/model/Book.java
@@ -6,12 +6,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Entity
 @Data
 @Builder
+@AllArgsConstructor
 public class Book {
 
     @Id
