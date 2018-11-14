3a3b961e5dd4637ac6713c18114d0c0c80cb101e
Patricia Sauer <patricia.sauer@maibornwolff.de>
2018-11-14 20:22:51 +0100

    Removed Configuration and and put everything into GraphqlApplication

--- src/main/java/com/psauer/graphql/GraphqlApplication.java
+++ src/main/java/com/psauer/graphql/GraphqlApplication.java
@@ -1,7 +1,23 @@
 package com.psauer.graphql;
 
+import com.psauer.graphql.exception.GraphQLErrorAdapter;
+import com.psauer.graphql.persistence.AuthorRepository;
+import com.psauer.graphql.persistence.BookRepository;
+import com.psauer.graphql.resolver.BookResolver;
+import com.psauer.graphql.resolver.Mutation;
+import com.psauer.graphql.resolver.Query;
+import graphql.ExceptionWhileDataFetching;
+import graphql.GraphQLError;
+import graphql.servlet.GraphQLErrorHandler;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class GraphqlApplication {
@@ -9,4 +25,56 @@ public class GraphqlApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GraphqlApplication.class, args);
 	}
+
+	@Bean
+	public GraphQLErrorHandler errorHandler() {
+		return new GraphQLErrorHandler() {
+			@Override
+			public List<GraphQLError> processErrors(List<GraphQLError> errors) {
+				List<GraphQLError> clientErrors = errors.stream()
+						.filter(this::isClientError)
+						.collect(Collectors.toList());
+
+				List<GraphQLError> serverErrors = errors.stream()
+						.filter(e -> !isClientError(e))
+						.map(GraphQLErrorAdapter::new)
+						.collect(Collectors.toList());
+
+				List<GraphQLError> e = new ArrayList<>();
+				e.addAll(clientErrors);
+				e.addAll(serverErrors);
+				return e;
+			}
+
+			protected boolean isClientError(GraphQLError error) {
+				return !(error instanceof ExceptionWhileDataFetching || error instanceof Throwable);
+			}
+		};
+	}
+
+	@Bean
+	public BookResolver authorResolver(AuthorRepository authorRepository) {
+		return new BookResolver(authorRepository);
+	}
+
+	@Bean
+	public Query query(BookRepository bookRepository, AuthorRepository authorRepository) {
+		return new Query(bookRepository, authorRepository);
+	}
+
+	@Bean
+	public Mutation mutation(BookRepository bookRepository, AuthorRepository authorRepository) {
+		return new Mutation(bookRepository, authorRepository);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("*").allowedOrigins("http://localhost:*");
+			}
+		};
+	}
+
 }
